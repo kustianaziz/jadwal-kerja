937f0f0b20eadfd5ed096d633f40b3642b420dd1
Kustian <kustianaziz6@gmail.com>
2026-08-11 18:24:44 +0700

    Bypass all Laravel caches in Vercel to fix Laravel 12 provider loading

--- api/index.php
+++ api/index.php
@@ -1,3 +1,23 @@
 <?php
 // Vercel serverless entrypoint for Laravel
+putenv('APP_CONFIG_CACHE=/tmp/nonexistent_config.php');
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/nonexistent_config.php';
+$_SERVER['APP_CONFIG_CACHE'] = '/tmp/nonexistent_config.php';
+
+putenv('APP_SERVICES_CACHE=/tmp/nonexistent_services.php');
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/nonexistent_services.php';
+$_SERVER['APP_SERVICES_CACHE'] = '/tmp/nonexistent_services.php';
+
+putenv('APP_PACKAGES_CACHE=/tmp/nonexistent_packages.php');
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/nonexistent_packages.php';
+$_SERVER['APP_PACKAGES_CACHE'] = '/tmp/nonexistent_packages.php';
+
+putenv('APP_EVENTS_CACHE=/tmp/nonexistent_events.php');
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/nonexistent_events.php';
+$_SERVER['APP_EVENTS_CACHE'] = '/tmp/nonexistent_events.php';
+
+putenv('APP_ROUTES_CACHE=/tmp/nonexistent_routes.php');
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/nonexistent_routes.php';
+$_SERVER['APP_ROUTES_CACHE'] = '/tmp/nonexistent_routes.php';
+
 require __DIR__ . '/../public/index.php';
