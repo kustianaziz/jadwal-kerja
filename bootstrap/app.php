a466457610efd74a473c13aee51f826cf674d0fa
Kustian <kustianaziz6@gmail.com>
2026-08-11 18:03:23 +0700

    Fix Vercel 500 error by redirecting storage to /tmp

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -16,3 +16,17 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
+    $storage = '/tmp/storage';
+    if (!is_dir($storage)) {
+        mkdir($storage, 0777, true);
+        mkdir($storage . '/framework/cache/data', 0777, true);
+        mkdir($storage . '/framework/sessions', 0777, true);
+        mkdir($storage . '/framework/views', 0777, true);
+        mkdir($storage . '/logs', 0777, true);
+    }
+    $app->useStoragePath($storage);
+}
+
+return $app;
