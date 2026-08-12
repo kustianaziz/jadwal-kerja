957588813c2aa9db5dcc937f8b1b0ae66e0667de
Kustian <kustianaziz6@gmail.com>
2026-08-12 13:34:57 +0700

    Fix Vite asset base paths and redirect manifest.json and sw.js routes on vercel.json

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/jadwal_kerja/public/build/',
+    base: '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
